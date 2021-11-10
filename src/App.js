e8b432c2605a2a823c4926a7fbc7b86be6d34515
Gabz <gabrielrosso609@gmail.com>
2021-11-10 12:15:48 +0100

    Proudly created Routes and Navigation

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
-import Footer  from './Components/Footer';
+import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Home from './Components/Home';
 import Gallery from './Components/Gallery';
 import Contact from './Components/Contact';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Gallery />
-      <Contact />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
